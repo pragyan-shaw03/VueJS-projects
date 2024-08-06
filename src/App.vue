1ce3d4f4b0b31855d767cef6c5611a44610aa065
Pragyan Shaw <pragyanwork101@gmail.com>
2024-08-07 01:28:20 +0530

    filter option made available in mobile by changing bootstrap nav

--- src/App.vue
+++ src/App.vue
@@ -15,41 +15,41 @@
                         <li class="nav-item">
                             <a class="nav-link active" href="#">About</a>
                         </li>
-                        <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
-                        <ul class="navbar-nav">
-                          <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                              Filter
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-                              <li>
-                                <div class="form-check ms-4">
-                                  <input class="form-check-input" type="checkbox" @change="categoryClicked('Art')" id="flexCheckChecked">
-                                  <label class="form-check-label text-light" for="flexCheckChecked">
-                                    Art
-                                  </label>
-                                </div>
-                              </li>
-                              <li>
-                                <div class="form-check ms-4">
-                                  <input class="form-check-input" type="checkbox" @change="categoryClicked('Finance')" id="flexCheckChecked">
-                                  <label class="form-check-label text-light" for="flexCheckChecked">
-                                    Finance
-                                  </label>
-                                </div>
-                              </li>
-                              <li>
-                                <div class="form-check ms-4">
-                                  <input class="form-check-input" type="checkbox" @change="categoryClicked('Tech')" id="flexCheckDefault">
-                                  <label class="form-check-label text-light" for="flexCheckDefault">
-                                    Technology
-                                  </label>
-                                </div>
-                              </li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </div>
+                        <li class="nav-item">
+                          <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                              <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Filter
+                              </a>
+                              <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+                                <li>
+                                  <div class="form-check ms-4">
+                                    <input class="form-check-input" type="checkbox" @change="categoryClicked('Art')" id="flexCheckChecked">
+                                    <label class="form-check-label text-light" for="flexCheckChecked">
+                                      Art
+                                    </label>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="form-check ms-4">
+                                    <input class="form-check-input" type="checkbox" @change="categoryClicked('Finance')" id="flexCheckChecked">
+                                    <label class="form-check-label text-light" for="flexCheckChecked">
+                                      Finance
+                                    </label>
+                                  </div>
+                                </li>
+                                <li>
+                                  <div class="form-check ms-4">
+                                    <input class="form-check-input" type="checkbox" @change="categoryClicked('Tech')" id="flexCheckDefault">
+                                    <label class="form-check-label text-light" for="flexCheckDefault">
+                                      Technology
+                                    </label>
+                                  </div>
+                                </li>
+                              </ul>
+                            </li>
+                          </ul>
+                        </li>
 
                     </ul>
                     <form class="d-flex">
